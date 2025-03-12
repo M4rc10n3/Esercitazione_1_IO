b3d995ba52ac3d443be5bdc2bc5c5159731f1f35
Marco <modasso23@icloud.com>
2025-03-12 20:35:21 +0100

    Adding what we did during the lesson on 12th of March

--- Exercise0/main.cpp
+++ Exercise0/main.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <numbers>
+
 int main()
 {
+    
+    std::cout << std::numeric_limits<int>::min() << " "
+     << std::numeric_limits<int>::max() << std::endl;
+    std::cout << std::setprecision(16) << std::scientific;
+    std::cout << std::numbers::pi << std::endl;
     return 0;
+    
 }
